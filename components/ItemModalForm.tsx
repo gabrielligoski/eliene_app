b448c1ece09296995fe6f876d8ce0080ef931c92
gabrielligoski <gabriel.ligoski@gmail.com>
2024-11-04 15:15:01 -0300

    fix: fixed form select

--- components/ItemModalForm.tsx
+++ components/ItemModalForm.tsx
@@ -1,55 +1,57 @@
 import {View} from "react-native";
 import React from "react";
-import {Select, SelectItem} from "@ui-kitten/components";
+import {Select, SelectItem, Text} from "@ui-kitten/components";
 import {Controller} from "react-hook-form";
 
 const Tecidos = ['Linhão', 'Suede', 'Couro', 'Outro']
+const Tamanhos = ['40x40', '40x50', '50x50', 'Outro']
 
 const ItemModalForm = ({selectedItem, control}: { selectedItem: string, control: any }) => {
     switch (selectedItem) {
         case 'saia-de-mesa':
         case 'almofada':
             return (<View>
-                {/*<Controller*/}
-                {/*    name="tecido"*/}
-                {/*    control={control}*/}
-                {/*    render={({ field }) => <Select*/}
-                {/*        onSelect={idx => field.onChange(idx)}*/}
-                {/*        selectedIndex={field?.value}*/}
-                {/*        label={'Escolha seu tecido...'}*/}
-                {/*        {...field}*/}
-                {/*    >*/}
-                {/*        {Tecidos.map(tecido => <SelectItem title={tecido}/>)}*/}
-                {/*    </Select>}*/}
-                {/*/>*/}
-                {/*<Controller*/}
-                {/*    name="tamanho"*/}
-                {/*    control={control}*/}
-                {/*    render={({ field }) => <Select*/}
-                {/*        style={{marginVertical: 12}}*/}
-                {/*        {...field}*/}
-                {/*    >*/}
-                {/*        <SelectItem title='40x40'/>*/}
-                {/*        <SelectItem title='40x50'/>*/}
-                {/*        <SelectItem title='50x50'/>*/}
-                {/*        <SelectItem title='Outro'/>*/}
-                {/*    </Select>}*/}
-                {/*/>*/}
+                <Controller
+                    name="tecido"
+                    control={control}
+                    render={({ field: {onChange, value} }) => <Select
+                        onSelect={idx => onChange(idx)}
+                        selectedIndex={value}
+                        label={'Escolha seu tecido...'}
+                        value={value && Tecidos[value.row]}
+                        style={{marginVertical: 6}}
+                    >
+                        {Tecidos.map(tecido => <SelectItem key={tecido} title={<Text>{tecido}</Text>} />)}
+                    </Select>}
+                />
+                <Controller
+                    name="tamanho"
+                    control={control}
+                    render={({ field: {onChange, value} }) => <Select
+                        onSelect={idx => onChange(idx)}
+                        selectedIndex={value}
+                        label={'Escolha seu tamanho...'}
+                        value={value && Tamanhos[value.row]}
+                        style={{marginVertical: 6}}
+                    >
+                        {Tamanhos.map(tamanho => <SelectItem key={tamanho} title={<Text>{tamanho}</Text>} />)}
+                    </Select>}
+                />
             </View>)
         case 'travesseiro':
             return (<View>
-                {/*<Controller*/}
-                {/*    name="tecido"*/}
-                {/*    control={control}*/}
-                {/*    render={({ field }) => <Select*/}
-                {/*        {...field}*/}
-                {/*    >*/}
-                {/*        <SelectItem title='Linhão'/>*/}
-                {/*        <SelectItem title='Suede'/>*/}
-                {/*        <SelectItem title='Couro'/>*/}
-                {/*        <SelectItem title='Outro'/>*/}
-                {/*    </Select>}*/}
-                {/*/>*/}
+                <Controller
+                    name="tecido"
+                    control={control}
+                    render={({ field: {onChange, value} }) => <Select
+                        onSelect={idx => onChange(idx)}
+                        selectedIndex={value}
+                        label={'Escolha seu tecido...'}
+                        value={Tecidos[value.row]}
+                    >
+                        {Tecidos.map(tecido => <SelectItem key={tecido} title={<Text>{tecido}</Text>} />)}
+                    </Select>}
+                />
             </View>)
         default:
             return <></>
